14ff3af4fb339e35121be8d6742b89b228ed124a
Diego G <ferchod2010@gmail.com>
2024-10-04 19:03:58 -0500

    ejercicios js

---
+++ javascript/ejercicios.js
@@ -0,0 +1,15 @@
+// let nombre = prompt("ingrese su nombre");
+// let edad = prompt("ingrese su edad: ")
+// console.log("bienvenido su nombre es: ",nombre, " y su edad es: ",edad)
+
+// let num1 = parseInt(prompt("ingrese un numero: "));
+
+// let num2 = parseInt(prompt("ingrese uel segundo numero: "));
+
+// let total = num1 + num2;
+// console.log("la suma es: ",total)
+// console.log(typeof total)
+//---------------------------------------------------
+//1. calcular el area de un rectangulo solicitando datos al usuario
+//2. convertir grados Celsius a fahrenheit f = c * 3/9 + 32
+//3. calcular promedio de 3 calificaciones de un estudiante
--- javascript/fundamento.js
+++ javascript/fundamento.js
@@ -1,12 +1,39 @@
 
-console.log("Hola mundo")
-//como declarar variables
-let nombre = "Diego";
-console.log(nombre)
-nombre = "carlos";
-console.log(nombre)
+// console.log("Hola mundo")
+// //como declarar variables
+// let nombre = "Diego";
+// console.log(nombre)
+// nombre = "carlos";
+// console.log(typeof nombre)
 
-let num = 34;
-console.log(num)
+// let num = 34;
+// console.log(num)
+// num = 106;
+// console.log(num)
+// num = 13.67
+// console.log( typeof num)
 
+// const PI = 3.1416
+// console.log(typeof PI)
+// // PI = 1
+// // console.log(PI)
 
+// let indefinido;
+// console.log(typeof indefinido)
+
+// let valoVAcio = null;
+// console.log(typeof valoVAcio)
+//-------------------------------------------------------
+let desafio = "aprender javascript en 30 dias"
+
+console.log(desafio.length)// longitud de la cadena
+console.log(desafio.toUpperCase());//convierte minusculas en MAYUSCULAS
+console.log(desafio.toLowerCase());//convierte en minuscula todo
+console.log(desafio.slice(3))//corta las primeras palabras
+console.log(desafio.includes("diaes"))//verifica si existe esa palabra
+console.log(desafio.replace("javascript","python"))//remplaza una palabra
+console.log(desafio.charAt(1))//imprime el indice
+
+let texto1 = "bienvenidos a"
+let texto2 = "javascript"
+console.log(texto1.concat(" ",texto2))
